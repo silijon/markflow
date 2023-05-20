79705bf17dfa3d20637075b72267d26486701668
silijon <john.w.dennis@gmail.com>
2023-05-19 22:38:37 -0700

    making it work with relative paths

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
 
 const process = require('process');
+const path = require('path');
 const fs = require('fs');
 const browserSync = require('browser-sync');
 
-// set current working directory to the current file's directory
-process.chdir(__dirname);
-
 // get the file to watch from the command line
 const argv = require('minimist')(process.argv.slice(2));
-const myFile = argv.f || argv.file;
+
+// resolve absolute path from myFile
+const mdFileAbs = path.resolve(argv._[0]);
+console.log(mdFileAbs);
+
+// set current working directory to the current file's directory
+process.chdir(__dirname);
 
 // start browserSync
 browserSync({
     server: './markflow',
     ui: false,
-    files: [myFile],
+    files: [mdFileAbs],
     middleware: [
         {
             route: '/markdown',
             handle: function (req, res, next) {
                 res.setHeader('Content-Type', 'text/plain');
-                res.end(fs.readFileSync(myFile));
+                res.end(fs.readFileSync(mdFileAbs));
                 next();
             }
         }
