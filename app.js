89bdebe90646edccf10c448fe8a7964fc1a49073
silijon <john.w.dennis@gmail.com>
2023-05-19 21:02:03 -0700

    first commit!

---
+++ app.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var browserSync = require('browser-sync');
+
+var argv = require('minimist')(process.argv.slice(2));
+var myFile = argv.f || argv.file;
+
+browserSync({
+    server: 'markflow',
+    ui: false,
+    files: [myFile],
+    middleware: [
+        {
+            route: '/markdown',
+            handle: function (req, res, next) {
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(fs.readFileSync(myFile));
+                next();
+            }
+        }
+    ]
+});
